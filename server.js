b353eee93d4f6acbe23b12b202264d10c87b599d
zakonik <zakonik2417@gmail.com>
2021-05-29 17:16:23 -0400

    fixed styling problem with css on single-post

--- server.js
+++ server.js
@@ -5,11 +5,12 @@ const sequelize = require('./config/connection');
 const app = express();
 const PORT = process.env.PORT || 3001;
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
-const session = require('express-session');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const helpers = require('./utils/helpers');
 const hbs = exphbs.create({ helpers });
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+
 const sess = {
   secret: 'Super secret secret',
   cookie: {},
